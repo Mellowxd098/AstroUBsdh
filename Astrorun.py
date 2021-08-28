ff7f3d726ffc8397dc9261e2adf4f0ef7aec23b8
Mellowxd098 <88098034+Mellowxd098@users.noreply.github.com>
2021-08-28 19:17:04 +0530

    Update Astrorun.py

--- Astrorun.py
+++ Astrorun.py
@@ -1,7 +1,7 @@
-from os import system
+import os
 
-system(
-      "git clone https://github.com/AstroUB/AstroUB && cd AstroUB && python3 -m astro"
+os.system(
+      "git clone https://github.com/Mellowxd098/AstroUB.git /root/astro && mkdir /root/astro/bin/ && cd /root/astro/ && chmod +x /usr/local/bin/*  && python3 -m astro"
 )
 # if something is wrong 
 print("You Have Got Error While Deployment Report and Get Help from @Astro_HelpChat")
